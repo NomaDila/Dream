d61737176b22ddb7e1c68ab4f7478ccd97e1cf83
lidkaulidka <lidkaulidkakalidka12@mail.ru>
2021-01-18 20:15:36 +0300

    Hello

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        
+    }
+
+    public static boolean comparisonArrays(int[] array, int[] array1) {
+        System.out.println(Arrays.toString(array) + "\n" + Arrays.toString(array1));
+        return Arrays.equals(array, array1);
     }
 }

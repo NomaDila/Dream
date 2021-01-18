1e59cba42e46176ffd51b1f7d7b1d89c0bcdbec7
Den Mit <denmit343@gmail.com>
2021-01-18 20:17:49 +0300

    Метод удаления букв

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        
+    }
+
+    public static int [] letterDelete (String [] strings) {
+        for (int i = 0; i < strings.length; i++) {
+            strings[i] = strings[i].replaceAll("[a-zA-z]", "");
+        }
+        int[] stringsToInt = new int[strings.length];
+        for (int i = 0; i < stringsToInt.length; i++) {
+            stringsToInt[i] = Integer.parseInt(strings[i]);
+        }
+        return stringsToInt;
     }
 }

4e0d55ca9dd2432b1557f99fc5e03f02936b25a9
Mikhail Tatarinov <Nomad-90@mail.ru>
2021-01-18 20:32:41 +0300

    Task1

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,13 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
+        float[] arrFloat = {1.2f, 1.8f, 2.2f, 1.5f};
+        String[] arrString = {"1g4d3", "3sdfs12", "7d2d2"};
 
+        System.out.println(comparisonArrays(getIntegerFractionalPart(getFractinalPart(arrFloat)), letterDelete(arrString)));
     }
 
-    public static int [] letterDelete (String [] strings) {
+    public static int[] letterDelete(String[] strings) {
         for (int i = 0; i < strings.length; i++) {
             strings[i] = strings[i].replaceAll("[a-zA-z]", "");
         }
@@ -20,21 +23,21 @@ public class Main {
         System.out.println(Arrays.toString(array) + "\n" + Arrays.toString(array1));
         return Arrays.equals(array, array1);
     }
-  
-        public static float[] getFractinalPart(float[] input) {
-            float[] result = new float[input.length];
-            for (int i = 0; i < input.length; i++) {
-                result[i] = input[i] - (int) (input[i]);
-            }
-            return result;
-        }
 
-        public static int[] getIntegerFractionalPart (float[] input){
-            int[] result=new int[input.length];
-            for (int i = 0; i < input.length; i++) {
-                result[i]=(int)((input[i]-(int)input[i])*10);
-            }
-            return result;
+    public static float[] getFractinalPart(float[] input) {
+        float[] result = new float[input.length];
+        for (int i = 0; i < input.length; i++) {
+            result[i] = input[i] - (int) (input[i]);
+        }
+        return result;
+    }
+
+    public static int[] getIntegerFractionalPart(float[] input) {
+        int[] result = new int[input.length];
+        for (int i = 0; i < input.length; i++) {
+            result[i] = (int) ((input[i] - (int) input[i]) * 10);
+        }
+        return result;
 
     }
 
